3f481388bac8d2ec409aa9ee0aa04c6a7be74d9a
shaikabd <abcd123@gmail.com>
2024-07-17 16:32:18 +0530

    shellscript added

--- Roboshop-Docker/imageCreation.sh
+++ Roboshop-Docker/imageCreation.sh
@@ -1,4 +1,4 @@
-imageList = "web catalogue cart mongodb user mysql shipping payment"
-for i in $(imageList); do
+imageList =(web catalogue cart mongodb user mysql shipping payment)
+for i in $(imageList[@]); do
 docker build -t $i:1 .
 done 
